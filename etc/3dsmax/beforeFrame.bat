c80047b927234afba776e3deab0d786a6d060098
Shrinidhi Rao <shrinidhi@clickbeetle.in>
2012-05-11 10:27:42 +0530

    fixing

--- etc/3dsmax/beforeFrame.bat
+++ etc/3dsmax/beforeFrame.bat
@@ -1,2 +1,2 @@
-del /q "C:\Users\blue\AppData\Local\Autodesk\3dsMax\2011 - 64bit\enu\3dsmax.ini"
-mklink "C:\Users\blue\AppData\Local\Autodesk\3dsMax\2011 - 64bit\enu\3dsmax.ini" "X:\standard\Autodesk\3dsMax\2011 - 64bit\enu\3dsmax.ini"
+del /q 'C:\Users\blue\AppData\Local\Autodesk\3dsMax\2011 - 64bit\enu\3dsmax.ini'
+mklink 'C:\Users\blue\AppData\Local\Autodesk\3dsMax\2011 - 64bit\enu\3dsmax.ini" "X:\standard\Autodesk\3dsMax\2011 - 64bit\enu\3dsmax.ini'
