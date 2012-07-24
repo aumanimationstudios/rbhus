d357de41a6a479f26e311c6e5ceab10563e68ea6
Shrinidhi Rao <shrinidhi@clickbeetle.in>
2012-07-24 21:54:40 +0530

    editing tasks works now

--- rbhus/utils.py
+++ rbhus/utils.py
@@ -16,28 +16,28 @@ import db
 
 
 def getHostGroups():
-    try:
-      conn = db.connRbhus()
-      cursor = conn.cursor(db.dict)
-      cursor.execute("select groups from hostInfo group by groups")
-      rows = cursor.fetchall()
-      cursor.close()
-      conn.close()
-    except:
-      print("Error connecting to db")
-      return(0)
-    gtr = {}
-    retRows = []
-    if(rows):
-      for row in rows:
-        tmpRow = row['groups'].split(",")
-        for tr in tmpRow:
-          gtr[tr.rstrip().lstrip()] = 1
-      for gt in gtr.keys():
-        retRows.append(gt)
-      return(retRows)
-    else:
-      return(0)
+  try:
+    conn = db.connRbhus()
+    cursor = conn.cursor(db.dict)
+    cursor.execute("select groups from hostInfo group by groups")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+  except:
+    print("Error connecting to db 1")
+    return(0)
+  gtr = {}
+  retRows = []
+  if(rows):
+    for row in rows:
+      tmpRow = row['groups'].split(",")
+      for tr in tmpRow:
+        gtr[tr.rstrip().lstrip()] = 1
+    for gt in gtr.keys():
+      retRows.append(gt)
+    return(retRows)
+  else:
+    return(0)
       
 def getFileTypes():
   try:
@@ -48,7 +48,7 @@ def getFileTypes():
     cursor.close()
     conn.close()
   except:
-    print("Error connecting to db")
+    print("Error connecting to db 2")
     return(0)
   retRows = []  
   if(rows):
