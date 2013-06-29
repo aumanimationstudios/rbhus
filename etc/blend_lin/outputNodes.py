c4d32e977168a0bbd8f941d979736261bf63fe7a
Shrinidhi Rao K <shrinidhi@clickbeetle.in>
2013-06-29 09:50:38 +0530

    fixing

--- etc/blend_lin/outputNodes.py
+++ etc/blend_lin/outputNodes.py
@@ -12,9 +12,8 @@ for x in scenes:
         if(renderLayer != "renameRenderLayer"):
           if(bpy.data.scenes[x].node_tree.nodes[n].inputs[0].links[0].from_node.layer == renderLayer):
             bpy.data.scenes[x].node_tree.nodes[n].base_path = outputDir.rstrip("/") + "/" + bpy.data.scenes[x].node_tree.nodes[n].inputs[0].links[0].from_socket.name
-            
+            print("output path : "+ str(bpy.data.scenes[x].node_tree.nodes[n].base_path))
         else:
           bpy.data.scenes[x].node_tree.nodes[n].base_path = outputDir.rstrip("/") + "/" + bpy.data.scenes[x].node_tree.nodes[n].inputs[0].links[0].from_socket.name
-        print("output path : "+ str(bpy.data.scenes[x].node_tree.nodes[n].base_path))
       except:
         print("fuck!!! .. output node not connected : "+ str(n))
