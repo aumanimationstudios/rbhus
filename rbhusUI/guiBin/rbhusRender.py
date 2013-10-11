bf9c3af44d2978543e3c8d35c74102c4b1be774f
Shrinidhi Rao <shrinidhi666@gmail.com>
2013-10-11 08:22:59 +0530

    fixing

--- rbhusUI/guiBin/rbhusRender.py
+++ rbhusUI/guiBin/rbhusRender.py
@@ -96,13 +96,14 @@ class Ui_Form(rbhusRenderMain.Ui_MainWindow):
     
     
   def closeEvent(self,event):
-    QtGui.QMessageBox.about(self.form,"QUITING?","Minimizing to Tray . Please quit from the tray icon if you really want to quit!")
     event.ignore()
+    QtGui.QMessageBox.about(self.form,"QUITING?","Minimizing to Tray . Please quit from the tray icon if you really want to quit!")
     self.form.setVisible(False) 
     
   def hostStop(self):
-    self.hostDets.hStop()
     self.hostDets.hDisable()
+    self.hostDets.hStop()
+    
   
   def showMain(self,actReason):
     if(actReason == 2):
