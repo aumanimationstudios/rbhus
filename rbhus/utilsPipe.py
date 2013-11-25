67ce79cd235890f7eec10d9d29bb124a5379affb
Shrinidhi Rao <shrinidhi666@gmail.com>
2013-11-25 18:33:29 +0530

    fixing

--- rbhus/utilsPipe.py
+++ rbhus/utilsPipe.py
@@ -45,5 +45,45 @@ def getStageTypes():
   except:
     print(str(sys.exc_info()))
     return(0)
-  
+
+
+def getProjDefaults(self):
+  try:
+    rows = self.db_conn.execute("desc tasks",dictionary=True)
+    taskFieldss = {}
+    for row in rows:
+      taskFieldss[row['Field']] = row['Default']
+    return(taskFieldss)
+  except:
+    return(0)
+
+def setupProj(projType,projName,projOs,directory,admins,rbhusRender,rbhusRenderServer,desc):
+  dbconn = dbPipe.dbPipe()
+  pTypes = getProjTypes()
+  cScript = ""
+  for pT in pTypes:
+    if(pT['type'] == projType):
+      cScript = pT['scriptDir']
+      
+  os.environ['rp_projName_c'] = projName
+  os.environ['rp_projType_c'] = projType
+  os.environ['rp_projDirectory_c'] = directory
+  os.environ['rp_projAdmin_c'] = admins
+  os.environ['rp_projRender_c'] = rbhusRender
+  os.environ['rp_projRenderS_c'] = rbhusRenderServer
+  os.environ['rp_projOs_c'] = projOs
+  os.environ['rp_projDesc_c'] = desc
+  try:
+    dbconn.execute("insert into proj (projName,directory,admins,os,projType,rbhusRenderIntergration,rbhusRenderServer,description) \
+                    values ('"+ str(projName) +"', \
+                    '"+ str(directory) +"', \
+                    '"+ str(admins) +"', \
+                    '"+ str(projOs) +"', \
+                    '"+ str(projType) +"', \
+                    '"+ str(rbhusRender) +"', \
+                    '"+ str(rbhusRenderServer) +"', \
+                    '"+ str(desc) +"')")
+  except:
+    print(str(sys.exc_info()))
+    
   
