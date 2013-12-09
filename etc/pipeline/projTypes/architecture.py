e712f5e1b1950620d45451507e473be3b47593b8
Shrinidhi Rao <shrinidhi666@gmail.com>
2013-12-09 06:52:14 +0530

    fixing

--- etc/pipeline/projTypes/architecture.py
+++ etc/pipeline/projTypes/architecture.py
@@ -16,21 +16,33 @@ if(len(progPath) > 1):
   cwd = os.path.abspath(pwd)
 else:
   cwd = os.path.abspath(os.getcwd())
+  
+try:
+  rbhusLibPath = os.environ['rbhusLibPath']
+  sys.path.append(rbhusLibPath)
+  import utilsPipe
+except:
+  pass
+try:
+  rbhusUILibPath = os.environ['rbhusUILibPath']
+  sys.path.append(rbhusUILibPath)
+except:
+  pass
+print(cwd)
+sys.exit()
 
 
 
 
-
-
-projName = os.environ['rp_projName_c']
-projType = os.environ['rp_projType_c']
-directory = os.environ['rp_projDirectory_c']
-admins = os.environ['rp_projAdmin_c']
-rbhusRender =  os.environ['rp_projRender_c']
-rbhusRenderServer = os.environ['rp_projRenderS_c']
-aclUser = os.environ['rp_projAclUser_c']
-aclGroup = os.environ['rp_projAclGroup_c']
-desc = os.environ['rp_projDesc_c']
+projName = os.environ['rp_proj_projName']
+projType = os.environ['rp_proj_projType']
+directory = os.environ['rp_proj_directory']
+admins = os.environ['rp_proj_admins']
+rbhusRender =  os.environ['rp_proj_rbhusRenderIntegration']
+rbhusRenderServer = os.environ['rp_proj_rbhusRenderServer']
+aclUser = os.environ['rp_proj_aclUser']
+aclGroup = os.environ['rp_proj_aclGroup']
+desc = os.environ['rp_proj_description']
 diskServer = os.environ['rp_dirMaps_server'] 
 diskNfsExport = os.environ['rp_dirMaps_nfsServDir']
 diskNfsMount = os.environ['rp_dirMaps_nfsMountDir']
@@ -53,12 +65,8 @@ except:
 lib = projTypeLibrary.split(":")
 libdir = ""
 for x in lib:
-  if(re.search("^\$directory",x)):
-    libdir = libdir.rstrip(os.sep) + os.sep + diskNfsMount.lstrip(os.sep).rstrip(os.sep) 
-  elif(re.search("^\$projName",x)):
-    libdir = libdir.rstrip(os.sep) + os.sep + projName.lstrip(os.sep)
-  else:
-    libdir = libdir.rstrip(os.sep) + os.sep + x.lstrip(os.sep)
+  if(re.search("^\$",str(x))):
+    libdir = libdir.rstrip(os.sep) + os.sep + os.environ["rp_"+ str(x).lstrip("$")]
 if(libdir):
   try:
     os.makedirs(libdir)
@@ -69,12 +77,8 @@ if(libdir):
 share = projTypeShare.split(":")
 sharedir = ""
 for x in share:
-  if(re.search("^\$directory",x)):
-    sharedir = sharedir.rstrip(os.sep) + os.sep + diskNfsMount.lstrip(os.sep).rstrip(os.sep) 
-  elif(re.search("^\$projName",x)):
-    sharedir = sharedir.rstrip(os.sep) + os.sep + projName.lstrip(os.sep)
-  else:
-    sharedir = sharedir.rstrip(os.sep) + os.sep + x.lstrip(os.sep)
+  if(re.search("^\$",str(x))):
+    sharedir = sharedir.rstrip(os.sep) + os.sep + os.environ["rp_"+ str(x).lstrip("$")]
 if(sharedir):
   try:
     os.makedirs(sharedir)
@@ -85,12 +89,8 @@ if(sharedir):
 output = projTypeOutput.split(":")
 outputdir = ""
 for x in output:
-  if(re.search("^\$directory",x)):
-    outputdir = outputdir.rstrip(os.sep) + os.sep + diskNfsMount.lstrip(os.sep).rstrip(os.sep) 
-  elif(re.search("^\$projName",x)):
-    outputdir = outputdir.rstrip(os.sep) + os.sep + projName.lstrip(os.sep)
-  else:
-    outputdir = outputdir.rstrip(os.sep) + os.sep + x.lstrip(os.sep)
+  if(re.search("^\$",str(x))):
+    outputdir = outputdir.rstrip(os.sep) + os.sep + os.environ["rp_"+ str(x).lstrip("$")]
 if(outputdir):
   try:
     os.makedirs(outputdir)
--- rbhus/utilsPipe.py
+++ rbhus/utilsPipe.py
@@ -115,7 +115,7 @@ def getAdmins():
 
 
 # createdUser should come from an env variable set by the authPipe module
-def createProj(projType,projName,directory,admins,rbhusRenderIntergration,rbhusRenderServer,aclUser,aclGroup,dueDate,description):
+def createProj(projType,projName,directory,admins,rbhusRenderIntegration,rbhusRenderServer,aclUser,aclGroup,dueDate,description):
   if(os.environ['rbhusPipe_acl_user'] not in getAdmins()):
     utilsPipeLogger("User not allowed to create projects")
     return(0)
@@ -126,7 +126,7 @@ def createProj(projType,projName,directory,admins,rbhusRenderIntergration,rbhusR
   projDets['projName'] = projName if(projName) else pDefs['projName']
   projDets['directory'] = directory if(directory) else pDefs['directory']
   projDets['admins'] = admins if(admins) else pDefs['admins']
-  projDets['rbhusRenderIntergration'] = rbhusRenderIntergration if(rbhusRenderIntergration) else pDefs['rbhusRenderIntergration']
+  projDets['rbhusRenderIntegration'] = rbhusRenderIntegration if(rbhusRenderIntegration) else pDefs['rbhusRenderIntegration']
   projDets['rbhusRenderServer'] = rbhusRenderServer if(rbhusRenderServer) else pDefs['rbhusRenderServer']
   projDets['aclUser'] = aclUser if(aclUser) else pDefs['aclUser']
   projDets['aclGroup'] = aclGroup if(aclGroup) else pDefs['aclGroup']
@@ -148,7 +148,7 @@ def createProj(projType,projName,directory,admins,rbhusRenderIntergration,rbhusR
 
 
 #always called by the server
-def setupProj(projType,projName,directory,admins,rbhusRenderIntergration,rbhusRenderServer,aclUser,aclGroup,createdUser,dueDate,description):
+def setupProj(projType,projName,directory,admins,rbhusRenderIntegration,rbhusRenderServer,aclUser,aclGroup,createdUser,dueDate,description):
   dbconn = dbPipe.dbPipe()
   pTypes = getProjTypes()
   cScript = ""
@@ -156,29 +156,29 @@ def setupProj(projType,projName,directory,admins,rbhusRenderIntergration,rbhusRe
     if(pT['type'] == projType):
       cScript = pT['scriptDir']
       
-  os.environ['rp_projName_c'] = str(projName)
-  os.environ['rp_projType_c'] = str(projType)
-  os.environ['rp_projDirectory_c'] = str(directory)
-  os.environ['rp_projAdmin_c'] = str(admins)
-  os.environ['rp_projRender_c'] = str(rbhusRenderIntergration)
-  os.environ['rp_projRenderS_c'] = str(rbhusRenderServer)
-  os.environ['rp_projDesc_c'] = str(description)
-  os.environ['rp_projAclUser_c'] = str(aclUser)
-  os.environ['rp_projAclGroup_c'] = str(aclGroup)
-  os.environ['rp_projDueDate_c'] = str(dueDate)
-  os.environ['rp_projCreatedUser_c'] = str(createdUser)
+  os.environ['rp_proj_projName'] = str(projName)
+  os.environ['rp_proj_projType'] = str(projType)
+  os.environ['rp_proj_directory'] = str(directory)
+  os.environ['rp_proj_admins'] = str(admins)
+  os.environ['rp_proj_rbhusRenderIntegration'] = str(rbhusRenderIntegration)
+  os.environ['rp_proj_rbhusRenderServer'] = str(rbhusRenderServer)
+  os.environ['rp_proj_description'] = str(description)
+  os.environ['rp_proj_aclUser'] = str(aclUser)
+  os.environ['rp_proj_aclGroup'] = str(aclGroup)
+  os.environ['rp_proj_dueDate'] = str(dueDate)
+  os.environ['rp_proj_createdUser'] = str(createdUser)
 
   exportDirMaps(directory)
   exportProjTypes(projType)
   utilsPipeLogger(description)
   utilsPipeLogger(projName)
   try:
-    dbconn.execute("insert into proj (projName,directory,admins,projType,rbhusRenderIntergration,rbhusRenderServer,aclUser,aclGroup,createdUser,dueDate,createDate,description) \
+    dbconn.execute("insert into proj (projName,directory,admins,projType,rbhusRenderIntegration,rbhusRenderServer,aclUser,aclGroup,createdUser,dueDate,createDate,description) \
                     values ('"+ str(projName) +"', \
                     '"+ str(directory) +"', \
                     '"+ str(admins) +"', \
                     '"+ str(projType) +"', \
-                    '"+ str(rbhusRenderIntergration) +"', \
+                    '"+ str(rbhusRenderIntegration) +"', \
                     '"+ str(rbhusRenderServer) +"', \
                     '"+ str(aclUser) +"', \
                     '"+ str(aclGroup) +"', \
@@ -265,45 +265,7 @@ def exportProj(projName):
       return(1)
   
   
-#def setupSequence(seqDict):
-  #projName = str(seqDict['projName'])
-  #seqName = str(seqDict['sequenceName'])
-  #seqId = hashlib.sha256(projName +":"+ seqName)
-  
-  #projDets = getProjDetails(str(seqDict['projName']))
-  #dirMapsDets = getDirMapsDetails(str(projDets['directory']))
-  
-  #dbconn = dbPipe.dbPipe()
-  #try:
-    #dbconn.execute("insert into sequence (sequenceId,projName,sequenceName,admins,sFrame,eFrame,createDate,dueDate,createdUser,description) \
-                    #values('" \
-                    #+ str(seqId) +"','" \
-                    #+ str(seqDict['projName']) +"','" \
-                    #+ str(seqDict['sequenceName']) +"','" \
-                    #+ str(seqDict['admins']) +"','" \
-                    #+ str(seqDict['sFrame']) +"','" \
-                    #+ str(seqDict['eFrame']) +"','" \
-                    #+ str(MySQLdb.Timestamp.now()) +"','" \
-                    #+ str(seqDict['dueDate']) +"','" \
-                    #+ str(os.environ['rbhusPipe_acl_user']) +"','" \
-                    #+ str(seqDict['description']) +"')")
-  #except:
-    #utilsPipeLogger(str(sys.exc_info()))
-    #return(0)
-  
-  #if(sys.platform.find("linux") >= 0):
-    #try:
-      #os.makedirs(dirMapsDets['linuxMapping'].rstrip("/") +"/"+ seqDict['projName'] +"/"+ seqDict['sequenceName'])
-    #except:
-      #utilsPipeLogger(str(sys.exc_info()))
-    
-  #if(sys.platform.find("win") >= 0):
-    #try:
-      #os.makedirs(dirMapsDets['windowsMapping'].rstrip("/") +"/"+ seqDict['projName'] +"/"+ seqDict['sequenceName'])
-    #except:
-      #utilsPipeLogger(str(sys.exc_info()))
-  #return(1)    
-      
+
     
   
   
@@ -342,8 +304,35 @@ def setupSequenceScene(seqSceDict):
   return(1)    
   
 
+def getFieldValue(table,field,fkey,fvalue):
+  dbconn = dbPipe.dbPipe()
+  try:
+    rows = dbconn.execute("select "+ str(field) +" from "+ str(table) +" where +" str(fkey) +"='"+ str(fvalue) +"'",dictionary=True)
+    return(rows)
+  except:
+    utilsPipeLogger(str(sys.exc_info()))
+    return(0)
   
+# take in a pipe type path eg :  $table_field:test: 
+# first and second should be $proj_directory:$proj_projName
+def getAbsPath(pipePath):
+  pPaths = pipePath.split(":")
+  projName = pPaths[0]
+  projDets = getProjDetails(projName)
+  projDirMapsDets = getDirMapsDetails(projDets['directory'])
   
+  absPath = ""
+  for x in pPaths:
+    if(re.search("^\$",x)):
+      if(absPath):
+        if(sys.platform.find("linux")):
+          absPath = absPath.rstrip("/") +"/"+ os.environ["rp_"+ str(x).lstrip("$")]
+  
+
+def getAssDetails():
+  
+
+
 class assets(object):
   def register(self,assDetDict):
     projName = str(assDetDict['projName'])
@@ -355,13 +344,36 @@ class assets(object):
     fileType = str(assDetDict['fileType'])
     assPath = ""
     assId = ""
-    if(int(assDetDict['library'])):
+    if(assDetDict['assetType'] == "library"):
       if(not re.search("$default",str(assDetDict['stageType']))):
         if(not re.search("$default",str(assDetDict['nodeType']))):
           if(not re.search("$default",str(assDetDict['fileType']))):
             assPath = str(assDetDict['projName']) +":library:"+ str(assDetDict['stageType']) +":"+ str(assDetDict['nodeType']) +":"+ str(assDetDict['assName']) +":"+ str(assDetDict['fileType'])
           else:
             assPath = str(assDetDict['projName']) +":library:"+ str(assDetDict['stageType']) +":"+ str(assDetDict['nodeType']) +":"+ str(assDetDict['assName'])
+        else:
+          if(not re.search("$default",str(assDetDict['fileType']))):
+            assPath = str(assDetDict['projName']) +":library:"+ str(assDetDict['stageType']) +":"+ str(assDetDict['assName']) +":"+ str(assDetDict['fileType'])
+          else:
+            assPath = str(assDetDict['projName']) +":library:"+ str(assDetDict['stageType']) +":"+ str(assDetDict['assName'])
+      else:
+        if(not re.search("$default",str(assDetDict['nodeType']))):
+          if(not re.search("$default",str(assDetDict['fileType']))):
+            assPath = str(assDetDict['projName']) +":library:"+ str(assDetDict['nodeType']) +":"+ str(assDetDict['assName']) +":"+ str(assDetDict['fileType'])
+          else:
+            assPath = str(assDetDict['projName']) +":library:"+ str(assDetDict['nodeType']) +":"+ str(assDetDict['assName'])
+        else:
+          if(not re.search("$default",str(assDetDict['fileType']))):
+            assPath = str(assDetDict['projName']) +":library:"+ str(assDetDict['assName']) +":"+ str(assDetDict['fileType'])
+          else:
+            assPath = str(assDetDict['projName']) +":library:"+ str(assDetDict['assName'])
+    elif(assDetDict['assetType'] == "output"):
+      if(not re.search("$default",str(assDetDict['stageType']))):
+        if(not re.search("$default",str(assDetDict['nodeType']))):
+          if(not re.search("$default",str(assDetDict['fileType']))):
+            assPath = str(assDetDict['projName']) +":output:"+ str(assDetDict['stageType']) +":"+ str(assDetDict['nodeType']) +":"+ str(assDetDict['assName']) +":"+ str(assDetDict['fileType'])
+          else:
+            assPath = str(assDetDict['projName']) +":output:"+ str(assDetDict['stageType']) +":"+ str(assDetDict['nodeType']) +":"+ str(assDetDict['assName'])
         else:
           if(not re.search("$default",str(assDetDict['fileType']))):
             assPath = str(assDetDict['projName']) +":library:"+ str(assDetDict['stageType']) +":"+ str(assDetDict['assName']) +":"+ str(assDetDict['fileType'])
