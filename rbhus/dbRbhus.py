d956a2dff7a862b2dbb15037cf602c9760edca81
Shrinidhi Rao <shrinidhi666@gmail.com>
2014-01-02 11:38:38 +0530

    fixing username problem with systemd

--- rbhus/dbRbhus.py
+++ rbhus/dbRbhus.py
@@ -35,10 +35,18 @@ try:
 except:
   pass
 
+
+
 if(sys.platform.find("win") >= 0):
-  username = os.environ['USERNAME']
+  try:
+    username = os.environ['USERNAME']
+  except:
+    username = "nobody"
 if(sys.platform.find("linux") >= 0):
-  username = os.environ['USER']
+  try:
+    username = os.environ['USER']
+  except:
+    username = "nobody"
 
 LOG_FILENAME = logging.FileHandler(tempDir + os.sep +"rbhusDb_module_"+ username +"_"+ str(hostname) +".log")
   #LOG_FILENAME = logging.FileHandler('z:/pythonTestWindoze.DONOTDELETE/clientLogs/rbhusDb_'+ hostname +'.log')
