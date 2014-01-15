e75f4e166bcf2ccaa6b3fd915ccedbb60853431e
root <root@blue0666.bluepixels.in>
2014-01-16 01:21:43 +0530

    fixing

--- etc/system/linuxUpdate.py
+++ etc/system/linuxUpdate.py
@@ -14,26 +14,26 @@ def gentooUpdate():
     
   os.environ['PORTAGE_BINHOST'] = "http://"+ str(masterSystem) +"/"
   os.environ['SYNC'] = "rsync://" + str(masterSystem) +"/gentoo-portage"
-  syncPortage = os.system("emerge --sync | tee /tmp/rbhusSystemUpdates")
-  syncSets = os.system("rsync -av rsync://"+ str(masterSystem) +"/sets /var/lib/portage/ | tee -a /tmp/rbhusSystemUpdates")
-  syncLayman = os.system("rsync -av rsync://"+ str(masterSystem) +"/layman /var/lib/layman/ | tee -a /tmp/rbhusSystemUpdates")
-  syncCbOverlay = os.system("rsync -av rsync://"+ str(masterSystem) +"/cb_overlay /usr/local/portage/cb_overlay/ | tee -a /tmp/rbhusSystemUpdates")
-  syncEtcPortage = os.system("rsync -av rsync://"+ str(masterSystem) +"/etcportage /etc/portage/ | tee -a /tmp/rbhusSystemUpdates") 
-  syncSystemD = os.system("rsync -av rsync://"+ str(masterSystem) +"/etcsystemd /etc/systemd/ | tee -a /tmp/rbhusSystemUpdates")
-  syncSystemDsys = os.system("rsync -av rsync://"+ str(masterSystem) +"/syssystemd /usr/lib/systemd/ | tee -a /tmp/rbhusSystemUpdates")
-  syncRsyncD = os.system("rsync -av rsync://"+ str(masterSystem) +"/etcrsyncd /etc/ | tee -a /tmp/rbhusSystemUpdates")
-  syncKernels = os.system("rsync -av rsync://"+ str(masterSystem) +"/kernels /etc/kernels | tee -a /tmp/rbhusSystemUpdates")
+  syncPortage = os.system("emerge --sync |& tee /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
+  syncSets = os.system("rsync -av rsync://"+ str(masterSystem) +"/sets /var/lib/portage/ |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
+  syncLayman = os.system("rsync -av rsync://"+ str(masterSystem) +"/layman /var/lib/layman/ |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
+  syncCbOverlay = os.system("rsync -av rsync://"+ str(masterSystem) +"/cb_overlay /usr/local/portage/cb_overlay/ |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
+  syncEtcPortage = os.system("rsync -av rsync://"+ str(masterSystem) +"/etcportage /etc/portage/ |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0") 
+  syncSystemD = os.system("rsync -av rsync://"+ str(masterSystem) +"/etcsystemd /etc/systemd/ |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
+  syncSystemDsys = os.system("rsync -av rsync://"+ str(masterSystem) +"/syssystemd /usr/lib/systemd/ |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
+  syncRsyncD = os.system("rsync -av rsync://"+ str(masterSystem) +"/etcrsyncd /etc/ |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
+  syncKernels = os.system("rsync -av rsync://"+ str(masterSystem) +"/kernels /etc/kernels |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
   
   if(not syncKernels):
     os.system("mount /boot")
-    os.system("rsync -av /etc/kernels/boot/ /boot/ | tee -a /tmp/rbhusSystemUpdates")
-    os.system("tar -xvf /etc/kernels/modules -C / | tee -a /tmp/rbhusSystemUpdates")
-    os.system("grub2-mkconfig -o /boot/grub/grub.cfg | tee -a /tmp/rbhusSystemUpdates")
+    os.system("rsync -av /etc/kernels/boot/ /boot/ |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
+    os.system("tar -xvf /etc/kernels/modules -C / |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
+    os.system("grub2-mkconfig -o /boot/grub/grub.cfg |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
     
   else:
     sys.exit(1)
   if((not syncPortage) and (not syncSets) and (not syncLayman) and (not syncCbOverlay) and (not syncEtcPortage) and (not syncSystemD) and (not syncSystemDsys) and (not syncRsyncD)):
-    emerge = os.system("emerge --exclude sys-apps/baselayout --deep -g @world | tee -a /tmp/rbhusSystemUpdates")
+    emerge = os.system("emerge --exclude sys-apps/baselayout --deep -g @world |& tee -a /tmp/rbhusSystemUpdates ; test ${PIPESTATUS[0]} -eq 0")
     if(not emerge):
       sys.exit(0)
     else:
