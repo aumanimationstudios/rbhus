591b1fb85cc57b7f9c5188073fb861f05a074568
Shrinidhi Rao <root@blue0666.bluepixels.in>
2015-06-07 10:20:25 +0530

    fixing

---
+++ tools/cloning/clone/makeClone_ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mount /boot                                                                                                                                                                                                                        
+rm -frv /crap/gentooOnesis                                                                                                                                                                                                  
+mkdir /crap/gentooOnesis       
+
+/home/shrinidhi/bin/gitHub/sys/clone/makeClone.py --root / -t /crap/gentooOnesis  -l /blueprod/STOR1/CRAP.serv/:/crap/STOR1.crap,/blueprod/STOR1/backup1/:/crap/incoming -b -x proc,dev,sys,blueprod,mnt,/crap,/BACKUP/,run,/etc/udev/rules.d/,tmp,home,.config/,root/,.pulse/,.pulse-cookie,/etc/conf.d/hostname,etc/mtab -m root:root,sys:root,home:root,tmp:root%0777,/etc/udev/rules.d/:root,run:root,/dev/:root,/proc/:root,/crap:root,mnt/cdrom:root,mnt/livecd:root,/crap/LOCAL.crap:root%0777,/crap/versionCache:root%0777,/crap/mercurial:root%0777 -e var/tmp,var/log
+rsync -av /crap/gentooOnesis/etc/skel/ /crap/gentooOnesis/root/ ; chown root:root /crap/gentooOnesis/root/ -Rv
+
+
+
+
