b909f739bff8284b9c2df6a1612fd6fe8dc56db6
Shrinidh Rao <shrinidhi666@gmail.com>
2015-08-05 23:19:51 +0530

    fixing

--- tests/mediaServerTest/HttpServer.py
+++ tests/mediaServerTest/HttpServer.py
@@ -2,15 +2,22 @@ import SimpleHTTPServer
 import SocketServer
 
 PORT = 8089
+clientsAllowed = ["127.0.0.1","192.168.1.243"]
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
   pass
 
 class httpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
-      if()
+      print("IN THE CLIENT THREAD")
+      print(self.headers['user-agent'])
+      if(self.client_address[0] not in clientsAllowed):
+        print("CLIENT : "+ str(self.client_address[0]) +" : NOT ALLOWED")
+        return(0)
+      print(dir(SimpleHTTPServer.SimpleHTTPRequestHandler))
+      SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+Handler = httpHandler
 
 httpd = ThreadedTCPServer(("", PORT), Handler)
 
