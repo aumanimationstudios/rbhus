6b836538d3f5abe407089a68562926c7e60baf41
Shrinidhi Rao <shrinidhi666@gmail.com>
2015-08-05 19:45:22 +0530

    testing http server

--- tests/mediaServerTest/HttpServer.py
+++ tests/mediaServerTest/HttpServer.py
@@ -1,12 +1,18 @@
 import SimpleHTTPServer
 import SocketServer
 
-PORT = 80
+PORT = 8089
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+  pass
+
+class httpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+      if()
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-print(dir(Handler))
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd = ThreadedTCPServer(("", PORT), Handler)
 
 print "serving at port", PORT
 httpd.serve_forever()
