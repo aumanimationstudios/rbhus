cf865d7496b1c92a3a8487d7fdf408b7c04ccd0f
Shrinidh Rao <shrinidhi666@gmail.com>
2016-01-12 22:03:04 +0530

    fixing

--- tools/system/sync_ubuntu.sh
+++ tools/system/sync_ubuntu.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 mv -v --no-clobber /usr/share/polkit-1/actions/org.freedesktop.udisks2.policy /usr/share/polkit-1/actions/org.freedesktop.udisks2.policy.old
+chown bluepixels:bluepixels /usr/bin/firefox* ; chmod 700 /usr/bin/firefox*
+chown bluepixels:bluepixels /usr/bin/google-chrome* ; chmod 700 /usr/bin/google-chrome*
 rsync -av ubuntu/ /
 
