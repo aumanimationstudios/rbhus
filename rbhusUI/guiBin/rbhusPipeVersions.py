9af55d9c3d7b40b067422ec3b3b762d333f15eaf
shrinidhi <shrinidhi666@gmail.com>
2017-03-04 10:37:06 +0530

    fixing

--- rbhusUI/guiBin/rbhusPipeVersions.py
+++ rbhusUI/guiBin/rbhusPipeVersions.py
@@ -402,7 +402,7 @@ class Ui_Form(rbhusPipeVersionsMod.Ui_MainWindow):
 
   def messageBoxWarn(self, hard=False):
     msgbox = QtGui.QMessageBox()
-    msgbox.setText(unicode("NOT COMMITING !\nAsset not assigned to you!!!"))
+    msgbox.setText(unicode("NOT COMMITING !\nAsset not assigned to you!!!\nThank Mr. BHARATH NAG for this popup"))
     msgbox.setIconPixmap(QtGui.QPixmap(_fromUtf8(dirSelf.rstrip(os.sep).rstrip("guiBin").rstrip(os.sep).rstrip("rbhusUI").rstrip(os.sep)+ os.sep +"etc/icons/poop.png")))
     #noBut = QtGui.QPushButton("cancel")
     #yesBut = QtGui.QPushButton("yes")
