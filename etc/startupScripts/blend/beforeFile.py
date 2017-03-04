df61fead485d4d920181b401752cfae49d17f222
shrinidhi <shrinidhi666@gmail.com>
2017-03-04 14:26:49 +0530

    fixing

--- etc/startupScripts/blend/beforeFile.py
+++ etc/startupScripts/blend/beforeFile.py
@@ -7,8 +7,13 @@ bpy.context.user_preferences.filepaths.save_version=0
 bpy.context.user_preferences.system.use_scripts_auto_execute = True
 addon_utils.disable("ui_layer_manager")
 addon_utils.enable("ui_layer_manager")
-addon_utils.disable("bone_selection_groups")
-addon_utils.enable("bone_selection_groups")
+if(str(bpy.app.version).find("(2, 78") < 0 ):
+  bpy.context.user_preferences.filepaths.use_load_ui = False
+  addon_utils.disable("bone_selection_sets")
+  addon_utils.enable("bone_selection_sets")
+else:
+  addon_utils.disable("bone_selection_groups")
+  addon_utils.enable("bone_selection_groups")
 # addon_utils.disable("camera_add_title_safe")
 # addon_utils.enable("camera_add_title_safe")
 
