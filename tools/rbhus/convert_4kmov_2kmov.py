e4307c7e52af37933d9da37b329e2d5350dc47ee
shrinidhi <shrinidhi666@gmail.com>
2017-03-08 10:57:04 +0530

    fixing

---
+++ tools/rbhus/convert_4kmov_2kmov.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+__author__ = "Shrinidhi Rao"
+__license__ = "GPL"
+__email__ = "shrinidhi666@gmail.com"
+
+import sys
+import os
+import glob
+
+path = sys.argv[1]
+movs = glob.glob(os.path.join(path,"*.mov"))
+for x in movs:
+  print(x)
+
+
+
