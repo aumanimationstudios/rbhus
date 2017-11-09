ed8cf75274544380d171cde848230bc256d48dab
shrinidhi <shrinidhi666@gmail.com>
2017-11-09 12:17:50 +0530

    lots of changes

--- tools/rbhus/convert_exr_mov_rle.py
+++ tools/rbhus/convert_exr_mov_rle.py
@@ -28,6 +28,14 @@ exr = "_".join(exrs[-1].split(".")[0].split("_")[:-1]) + "_%04d.exr"
 startFrame = exrs[0].split("_")[-1].rstrip(".exr")
 endFrame = exrs[-1].split("_")[-1].rstrip(".exr")
 inputFileFmt = "_".join(exrs[-1].split(".")[0].split("_")[:-1]) + "_"+ startFrame +"-"+ endFrame +".exr"
+
+ffmpeg = None
+if(os.path.exists("/opt/lib/ffmpeg/bin/ffmpeg")):
+  ffmpeg = "/opt/lib/ffmpeg/bin/ffmpeg"
+else:
+  print("Not found : /opt/lib/ffmpeg/bin/ffmpeg")
+  ffmpeg = "ffmpeg"
+
 cmd = ffmpeg +" -probesize 50000000 -apply_trc iec61966_2_1 -r 24 -start_number "+ str(startFrame) +" -i "+ exr +" -c:v qtrle -qscale:v 5 -y "+ mov
 # cmd = "djv_convert "+ inputFileFmt +" "+ mov +" -default_speed 24 -render_filter_high"
 p = subprocess.Popen(cmd,shell=True)
--- tools/rbhus/convert_exr_mp4.py
+++ tools/rbhus/convert_exr_mp4.py
@@ -28,7 +28,16 @@ exr = "_".join(exrs[-1].split(".")[0].split("_")[:-1]) + "_%04d.exr"
 startFrame = exrs[0].split("_")[-1].rstrip(".exr")
 endFrame = exrs[-1].split("_")[-1].rstrip(".exr")
 inputFileFmt = "_".join(exrs[-1].split(".")[0].split("_")[:-1]) + "_"+ startFrame +"-"+ endFrame +".exr"
-cmd = "djv_convert "+ inputFileFmt +" "+ mp4 +" -default_speed 24 -render_filter_high"
+
+ffmpeg = None
+if(os.path.exists("/opt/lib/ffmpeg/bin/ffmpeg")):
+  ffmpeg = "/opt/lib/ffmpeg/bin/ffmpeg"
+else:
+  print("Not found : /opt/lib/ffmpeg/bin/ffmpeg")
+  ffmpeg = "ffmpeg"
+
+cmd = ffmpeg +" -probesize 50000000 -apply_trc iec61966_2_1 -r 24 -start_number "+ str(startFrame) +" -i "+ exr +" -c:v x264 -qscale:v 5 -y "+ mp4
+# cmd = "djv_convert "+ inputFileFmt +" "+ mp4 +" -default_speed 24 -render_filter_high"
 p = subprocess.Popen(cmd,shell=True)
 p.wait()
 # os.remove(unatron)
