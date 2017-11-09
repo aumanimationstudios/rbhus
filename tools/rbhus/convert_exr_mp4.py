5270c87b0f286e4329b6bb2cf65ec4b72072bb82
shrinidhi <shrinidhi666@gmail.com>
2017-11-09 12:32:59 +0530

    lots of changes

--- tools/rbhus/convert_exr_mp4.py
+++ tools/rbhus/convert_exr_mp4.py
@@ -36,7 +36,7 @@ else:
   print("Not found : /opt/lib/ffmpeg/bin/ffmpeg")
   ffmpeg = "ffmpeg"
 
-cmd = ffmpeg +" -probesize 50000000 -apply_trc iec61966_2_1 -r 24 -start_number "+ str(startFrame) +" -i "+ exr +" -c:v h264 -qscale:v 5 -y "+ mp4
+cmd = ffmpeg +" -probesize 50000000 -apply_trc iec61966_2_1 -r 24 -start_number "+ str(startFrame) +" -i "+ exr +" -c:v libx264 -pix_fmt yuv420p -crf 20 -y "+ mp4
 # cmd = "djv_convert "+ inputFileFmt +" "+ mp4 +" -default_speed 24 -render_filter_high"
 p = subprocess.Popen(cmd,shell=True)
 p.wait()
