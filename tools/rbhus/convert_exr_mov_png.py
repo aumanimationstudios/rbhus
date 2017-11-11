2c105fdc14eca6698c9bcac2601003d57fe9af62
shrinidhi <shrinidhi666@gmail.com>
2017-11-11 17:26:52 +0530

    lots of changes

--- tools/rbhus/convert_exr_mov_png.py
+++ tools/rbhus/convert_exr_mov_png.py
@@ -36,7 +36,7 @@ else:
   print("Not found : /opt/lib/ffmpeg/bin/ffmpeg")
   ffmpeg = "ffmpeg"
 
-cmd = ffmpeg +" -probesize 50000000 -apply_trc iec61966_2_1 -r 24 -start_number "+ str(startFrame) +" -i "+ exr +" -c:v png -pix_fmt rgb24 -qscale:v 5 -y "+ mov
+cmd = ffmpeg +" -probesize 50000000 -apply_trc iec61966_2_1 -r 24 -start_number "+ str(startFrame) +" -i "+ exr +" -c:v png -pix_fmt rgb24 -qscale:v 1 -y "+ mov
 # cmd = "djv_convert "+ inputFileFmt +" "+ mov +" -default_speed 24 -render_filter_high"
 p = subprocess.Popen(cmd,shell=True)
 p.wait()
