a9ce6c2db1de6ce1784a94b7399840ce962bd74e
shrinidhi <shrinidhi666@gmail.com>
2017-12-02 23:00:46 +0530

    lots of changes

--- tools/cloning/clone/makeClone_gentoo.sh
+++ tools/cloning/clone/makeClone_gentoo.sh
@@ -4,7 +4,7 @@ mount /boot
 rm -frv /crap/gentooOnesis-gentoo
 mkdir /crap/gentooOnesis-gentoo
 
-/home/shrinidhi/bin/gitHub/sys/clone/makeClone.py --root / -t /crap/gentooOnesis-gentoo  -l /blueprod/STOR2/stor2/stor1/crap:/crap/STOR1.crap -b -x root,usr/portage,proc,dev,sys,blueprod,mnt,/crap,/BACKUP/,run,/etc/udev/rules.d/,tmp,home,.config/,.pulse/,.pulse-cookie,/etc/conf.d/hostname,etc/mtab -m root:root,sys:root,home:root,tmp:root%0777,/etc/udev/rules.d/:root,run:root,/dev/:root,/proc/:root,crap:root,mnt/cdrom:root,mnt/livecd:root,crap/LOCAL.crap:root%0777,crap/versionCache:root%0777,crap/mercurial:root%0777 -e var/tmp,var/log,opt/home/bluepixels
+/home/shrinidhi/bin/gitHub/sys/clone/makeClone.py --root / -t /crap/gentooOnesis-gentoo  -l /blueprod/CRAP/crap:/crap/crap.server -b -x root,usr/portage,proc,dev,sys,blueprod,mnt,/crap,/BACKUP/,run,/etc/udev/rules.d/,tmp,home,.config/,.pulse/,.pulse-cookie,/etc/conf.d/hostname,etc/mtab -m root:root,sys:root,home:root,tmp:root%0777,/etc/udev/rules.d/:root,run:root,/dev/:root,/proc/:root,crap:root,mnt/cdrom:root,mnt/livecd:root,crap/LOCAL.crap:root%0777,crap/versionCache:root%0777,crap/mercurial:root%0777 -e var/tmp,var/log,opt/home/bluepixels
 cp -av /etc/fstab.clone /crap/gentooOnesis-gentoo/etc/fstab
 ssh clonemaster -C "rm -fr /crap/gentooOnesis-gentoo"
 rsync -avHX /crap/gentooOnesis-gentoo clonemaster:/crap/
