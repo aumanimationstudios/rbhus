9c809a92eda82d5cffb09cf0f91b06b2f83e61d8
shrinidhi <shrinidhi666@gmail.com>
2018-03-30 15:21:45 +0530

    fixing gif generator for better quality

--- tools/convert-to-gif.sh
+++ tools/convert-to-gif.sh
@@ -5,5 +5,5 @@ filtersPalete="fps=24,scale=640:-1:flags=lanczos"
 filtersGif="fps=24,scale=640:-1:flags=lanczos"
 
 #ffmpeg -ss 00:00:01 -i $1 -vf "$filtersPalete,palettegen=stats_mode=diff" -y $palette
-ffmpeg -ss 00:00:01 -i $1 -vf "$filtersPalete,palettegen=stats_mode=diff" -y $palette
+ffmpeg -ss 00:00:01 -i $1 -vf "$filtersPalete,palettegen=stats_mode=full" -y $palette
 ffmpeg -i $1 -i $palette -lavfi "$filtersGif [x]; [x][1:v] paletteuse=dither=bayer:bayer_scale=3" -y $1.gif
