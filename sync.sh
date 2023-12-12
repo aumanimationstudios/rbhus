b9f7f19e173962594d146c88269058dbee1aaf9b
sanath111 <sanathshetty111@gmail.com>
2023-12-12 08:35:17 +0530

    fixes to sync

--- sync.sh
+++ sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir -p /dell1-pool/tempprojdump
-mount stor1:/dell1-pool/stor2/stor1/prod/projdump /dell1-pool/tempprojdump
-mv /dell1-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus /dell1-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus.`date +%Y_%B_%A_%d_%H-%M-%S`
-rsync -av ./ /dell1-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus/ --delete --exclude=*.pyc --exclude=.git
-cd /dell1-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus/ ; ./clean.sh ; cd -
-chown root:root /dell1-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus/ -Rv
-chmod 755 /dell1-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus/ -Rv
-umount /dell1-pool/tempprojdump
+mkdir -p /dell2-pool/tempprojdump
+mount stor6:/dell2-pool/stor6/stor1/prod/projdump /dell2-pool/tempprojdump
+mv /dell2-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus /dell2-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus.`date +%Y_%B_%A_%d_%H-%M-%S`
+rsync -av ./ /dell2-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus/ --delete --exclude=*.pyc --exclude=.git
+cd /dell2-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus/ ; ./clean.sh ; cd -
+chown root:root /dell2-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus/ -Rv
+chmod 755 /dell2-pool/tempprojdump/pythonTestWindoze.DONOTDELETE/rbhus/ -Rv
+umount /dell2-pool/tempprojdump
 
 
