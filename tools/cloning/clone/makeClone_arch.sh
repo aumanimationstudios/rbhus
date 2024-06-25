d9529c96e8a7d98434080ddf24b6ab5e7e5159c8
sanath111 <sanathshetty111@gmail.com>
2024-06-25 16:29:11 +0530

    fixes to makeclone

--- tools/cloning/clone/makeClone_arch.sh
+++ tools/cloning/clone/makeClone_arch.sh
@@ -4,7 +4,7 @@ mount /boot
 rm -frv /crap/gentooOnesis-arch
 mkdir /crap/gentooOnesis-arch
 
-/home/sanath/git_repos/rbhus/tools/cloning/clone/makeClone.py --root / -t /crap/gentooOnesis-arch  -l /blueprod/CRAP/crap:/crap/crap.server -b -x root,var/cache/pacman,proc,dev,sys,blueprod,mnt,/crap,/BACKUP/,run,/etc/udev/rules.d/,tmp,home,.config/,.pulse/,.pulse-cookie,/etc/conf.d/hostname,etc/mtab -m root:root,sys:root,home:root,tmp:root%0777,/etc/udev/rules.d/:root,run:root,/dev/:root,/proc/:root,crap:root,mnt/cdrom:root,mnt/livecd:root,crap/LOCAL.crap:root%0777,crap/versionCache:root%0777,crap/mercurial:root%0777 -e var/tmp,var/log,opt/home/bluepixels
+/opt/rbhus/tools/cloning/clone/makeClone.py --root / -t /crap/gentooOnesis-arch  -l /blueprod/CRAP/crap:/crap/crap.server -b -x root,var/cache/pacman,proc,dev,sys,blueprod,mnt,/crap,/BACKUP/,run,/etc/udev/rules.d/,tmp,home,.config/,.pulse/,.pulse-cookie,/etc/conf.d/hostname,etc/mtab -m root:root,sys:root,home:root,tmp:root%0777,/etc/udev/rules.d/:root,run:root,/dev/:root,/proc/:root,crap:root,mnt/cdrom:root,mnt/livecd:root,crap/LOCAL.crap:root%0777,crap/versionCache:root%0777,crap/mercurial:root%0777 -e var/tmp,var/log,opt/home/bluepixels
 cp -av /etc/fstab.clone /crap/gentooOnesis-arch/etc/fstab
 ssh clonemaster -C "rm -fr /crap/gentooOnesis-arch"
 rsync -avHX /crap/gentooOnesis-arch clonemaster:/crap/
