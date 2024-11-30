6fccc9bf93e684ea8f280db707a80019b52c732c
sanath111 <sanathshetty111@gmail.com>
2024-11-30 07:58:33 +0530

    updated render default blender to 4.2

--- etc/blend_lin_tj/default.py
+++ etc/blend_lin_tj/default.py
@@ -46,7 +46,8 @@ camera = os.environ['rbhus_camera']
 res = os.environ['rbhus_resolution']
 
 # RENDERCMD = "/usr/local/bin/blender -noaudio -b \"" + str(fileName) +"\""
-RENDERCMD = "/usr/local/bin/blender_flatpak -noaudio -b \"" + str(fileName) +"\""
+# RENDERCMD = "/usr/local/bin/blender_flatpak -noaudio -b \"" + str(fileName) +"\""
+RENDERCMD = "/usr/local/bin/blender_4_2 -noaudio --factory-startup -b  \"" + str(fileName) +"\""
 
 # if("rbhus_renExtEnv" in rbhus.renderPlugin.env):
 #   extEnv = rbhus.renderPlugin.env['rbhus_renExtEnv']
